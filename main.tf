710e8e786b8576cf9658d2b1267391de4dad4df2
HaimBD <haimbendavid1995@gmail.com>
2025-08-09 14:23:17 +0300

    Editing main.tf - 09.08.2025

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "4.16"
-#     }
-#   }
-#
-#   required_version = ">= 1.2.0"
-# }
-#
-#
-#
-#
-#
-# provider "aws" {
-#   region  = "<aws-region>"
-#   profile = "<aws-course-profile>"
-# }
-
 # Setting provider for Terraform API's
 provider "aws" {
   region = "us-east-1"
